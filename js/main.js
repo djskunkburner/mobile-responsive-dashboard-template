878806faeb6e47569c03681cd654f50c37740648
djskunkburner <31620826+djskunkburner@users.noreply.github.com>
2019-02-25 11:09:24 +1000

    sidenav open/close working on mobile view

--- js/main.js
+++ js/main.js
@@ -7,3 +7,26 @@ if ("serviceWorker" in navigator) {
       .catch(err => console.log(`Service Worker: Error: ${err}`));
   });
 }
+
+const menuIconEl = $(".menu-icon");
+const sidenavEl = $(".sidenav");
+const sidenavCloseEl = $(".sidenav__close-icon");
+
+// Add and remove provided class names
+function toggleClassName(el, className) {
+  if (el.hasClass(className)) {
+    el.removeClass(className);
+  } else {
+    el.addClass(className);
+  }
+}
+
+// Open the sidenav on click
+menuIconEl.on("click", function() {
+  toggleClassName(sidenavEl, "active");
+});
+
+// Close the sidenav on click
+sidenavCloseEl.on("click", function() {
+  toggleClassName(sidenavEl, "active");
+});
